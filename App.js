f7bba93bebe8433fe14296b99c1a14f23f406102
JACOB SINGER <jacobbradleysinger@gmail.com>
2022-10-15 12:46:19 -0500

    general project

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React in our code
+import React, { useState } from 'react';
 
-export default function App() {
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+//importing library to use Stopwatch and Timer
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
+
+const App = () => {
+  const [isTimerStart, setIsTimerStart] = useState(false);
+  const [timerDuration, setTimerDuration] = useState(2000);
+  const [resetTimer, setResetTimer] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.sectionStyle}>
+          <Timer
+            totalDuration={timerDuration}
+            msecs
+            //Time Duration
+            start={isTimerStart}
+            //To start
+            reset={resetTimer}
+            //To reset
+            options={options}
+            //options for the styling
+            handleFinish={() => {
+              alert('Custom Completion Function');
+            }}
+            //can call a function On finish of the time
+            getTime={(time) => {
+              console.log(time);
+            }}
+          />
+          <TouchableHighlight
+            onPress={() => {
+              setIsTimerStart(!isTimerStart);
+              setResetTimer(false);
+            }}>
+            <Text style={styles.buttonText}>
+              {!isTimerStart ? 'START' : 'STOP'}
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              setIsTimerStart(false);
+              setResetTimer(true);
+            }}>
+            <Text style={styles.buttonText}>RESET</Text>
+          </TouchableHighlight>
+          <Picker
+            style={{ height: 50, width: 150 }}
+            selectedValue={selectedLanguage}
+            mode='dropdown'
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedLanguage(itemValue)
+            }>
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+          </Picker>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionStyle: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonText: {
+    fontSize: 20,
+    marginTop: 10,
+  },
 });
+
+const options = {
+  container: {
+    backgroundColor: '#FF0000',
+    padding: 5,
+    borderRadius: 5,
+    width: 200,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 25,
+    color: '#FFF',
+    marginLeft: 7,
+  },
+};
