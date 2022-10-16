511f198c5394913ab28d3ef4228ad12f2ca47f82
JACOB SINGER <jacobbradleysinger@gmail.com>
2022-10-16 15:00:31 -0500

    updated button styling

--- App.js
+++ App.js
@@ -16,6 +16,8 @@ const App = () => {
   const [randomTimeValue, setrandomTimeValue] = useState(Math.random());
   const [timerDuration, setTimerDuration] = useState(timeInMsecs * timeType * randomTimeValue);
   const [resetTimer, setResetTimer] = useState(false);
+  const [timerButtonPressed, setTimerButtonPressed] = useState(false);
+  const [randomButtonPressed, setRandomButtonPressed] = useState(false);
 
   useEffect(() => {
     // Without timeInMsecs as a placeholder then timeDuration recursively stacks on itself...
@@ -26,87 +28,97 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.sectionStyle}>
-        <View style={{ alignItems: 'center', position: 'absolute', top: 70 }}>
-          <View style={{ marginVertical: 10 }}>
-            <Timer
-              totalDuration={timerDuration}
-              msecs
-              start={isTimerStart}
-              reset={resetTimer}
-              options={options}
-              handleFinish={() => {
+        <Timer
+          totalDuration={timerDuration}
+          msecs
+          start={isTimerStart}
+          reset={resetTimer}
+          options={options}
+          handleFinish={() => {
+            setResetTimer(true);
+            setIsTimerStart(!isTimerStart);
+            alert("Timer finished");
+          }}
+          getTime={(time) => {
+            console.log(time);
+          }}
+        />
+        <View style={{ alignItems: 'center', position: 'absolute', top: 70, flexDirection: 'row' }}>
+          <TouchableHighlight
+            underlayColor={'gray'}
+            onPressOut={() => {
+              setTimerButtonPressed(false);
+            }}
+            onPressIn={() => {
+              setTimerButtonPressed(true);
+            }}
+            onPress={() => {
+              setIsTimerStart(!isTimerStart);
+              setResetTimer(false);
+              if (isTimerStart)
                 setResetTimer(true);
-                setIsTimerStart(!isTimerStart);
-                alert("Timer finished");
-              }}
-              getTime={(time) => {
-                console.log(time);
-              }}
-            />
-          </View>
-          <View style={{ marginVertical: 10, borderWidth: 3, borderColor: 'black', paddingHorizontal: 20, paddingBottom: 5 }}>
+            }}>
+            <Text style={timerButtonPressed ? styles.timerButtonPressedText : styles.buttonText}>
+              {!isTimerStart ? 'START' : 'STOP'}
+            </Text>
+          </TouchableHighlight>
+          <View style={{ ...styles.containerRow, paddingBottom: 10, paddingHorizontal: 5 }}>
             <TouchableHighlight
               underlayColor={'gray'}
-              onPress={() => {
-                setIsTimerStart(!isTimerStart);
-                setResetTimer(false);
-                if (isTimerStart)
-                  setResetTimer(true);
-              }}>
-              <Text style={styles.buttonText}>
-                {!isTimerStart ? 'START' : 'STOP'}
-              </Text>
-            </TouchableHighlight>
-          </View>
-          <View style={{ marginVertical: 10, borderWidth: 3, borderColor: 'black', paddingHorizontal: 20, paddingBottom: 5 }}>
-            <TouchableHighlight
-              underlayColor={'gray'}
+              onPressOut={() => {
+                setRandomButtonPressed(false);
+              }}
+              onPressIn={() => {
+                setRandomButtonPressed(true);
+              }}
               onPress={() => {
                 setIsTimerStart(false);
                 setResetTimer(true);
                 setrandomTimeValue(Math.random());
                 setTimerDuration(timeInMsecs * timeType * randomTimeValue);
               }}>
-              <Text style={styles.buttonText}>RANDOMIZE</Text>
+              <Text style={randomButtonPressed ? styles.timerButtonPressedText : styles.buttonText}> RANDOMIZE</Text>
             </TouchableHighlight>
           </View>
         </View>
-        <View style={{ alignItems: 'flex-start', position: 'absolute', bottom: 200 }}>
-          <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 40, borderWidth: 3, borderColor: 'black' }}>
-            <Text style={{ fontSize: 20 }}> Time Duration </Text>
-            <Picker
-              style={{ height: 50, width: 150 }}
-              selectedValue={timeInMsecs}
-              mode='dropdown'
-              onValueChange={(itemValue, itemIndex) =>
-                setTimeInMsecs(itemValue)
-              }>
-              <Picker.Item label="5" value={5} />
-              <Picker.Item label="10" value={10} />
-              <Picker.Item label="20" value={20} />
-              <Picker.Item label="30" value={30} />
-              <Picker.Item label="40" value={40} />
-              <Picker.Item label="50" value={50} />
-              <Picker.Item label="60" value={60} />
-            </Picker>
-          </View>
-          <View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 3, borderColor: 'black' }}>
-            <Text style={{ fontSize: 20 }}> Time Unit </Text>
-            <Picker
-              style={{ height: 50, width: 150 }}
-              selectedValue={timeType}
-              mode='dropdown'
-              onValueChange={(itemValue, itemIndex) =>
-                setTimeType(itemValue)
-              }>
-              <Picker.Item label="seconds" value={1000} />
-              <Picker.Item label="minutes" value={60000} />
-              <Picker.Item label="hours" value={3600000} />
-            </Picker>
+        <View style={{ alignItems: 'center', position: 'absolute', bottom: 200 }}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.containerRow}>
+              <Text style={{ fontSize: 20 }}> Time Duration </Text>
+              <Picker
+                style={{ height: 50, width: 25 }}
+                selectedValue={timeInMsecs}
+                mode='dropdown'
+                onValueChange={(itemValue, itemIndex) =>
+                  setTimeInMsecs(itemValue)
+                }>
+                <Picker.Item label="5" value={5} />
+                <Picker.Item label="10" value={10} />
+                <Picker.Item label="20" value={20} />
+                <Picker.Item label="30" value={30} />
+                <Picker.Item label="40" value={40} />
+                <Picker.Item label="50" value={50} />
+                <Picker.Item label="60" value={60} />
+              </Picker>
+            </View>
+            <View style={styles.containerRow}>
+              <Text style={{ fontSize: 20 }}> Time Unit </Text>
+              <Picker
+                style={{ height: 50, width: 25 }}
+                selectedValue={timeType}
+                mode='dropdown'
+                onValueChange={(itemValue, itemIndex) =>
+                  setTimeType(itemValue)
+                }>
+                <Picker.Item label="seconds" value={1000} />
+                <Picker.Item label="minutes" value={60000} />
+                <Picker.Item label="hours" value={3600000} />
+              </Picker>
+            </View>
           </View>
         </View>
       </View>
-    </SafeAreaView>
+    </SafeAreaView >
   );
 };
 
@@ -128,6 +140,17 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 10,
   },
+  timerButtonPressedText: {
+    fontSize: 15,
+    marginTop: 10,
+  },
+  containerRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 20,
+    borderWidth: 3,
+    borderColor: 'black'
+  }
 });
 
 const options = {
