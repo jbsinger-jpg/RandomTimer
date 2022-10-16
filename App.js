3cf27c811ca7e1d7ab3a15babe5e975562c1cb33
JACOB SINGER <jacobbradleysinger@gmail.com>
2022-10-16 16:00:53 -0500

    update

--- App.js
+++ App.js
@@ -18,7 +18,8 @@ const App = () => {
   const [resetTimer, setResetTimer] = useState(false);
   const [timerButtonPressed, setTimerButtonPressed] = useState(false);
   const [randomButtonPressed, setRandomButtonPressed] = useState(false);
-
+  const [timeUnitButtonPressed, setTimeUnitButtonPressed] = useState(false);
+  const [timeDurationButtonPressed, setTimeDurationButtonPressed] = useState(false);
   useEffect(() => {
     // Without timeInMsecs as a placeholder then timeDuration recursively stacks on itself...
     setrandomTimeValue(Math.random());
@@ -84,36 +85,54 @@ const App = () => {
         <View style={{ alignItems: 'center', position: 'absolute', bottom: 200 }}>
           <View style={{ flexDirection: 'row' }}>
             <View style={styles.containerRow}>
-              <Text style={{ fontSize: 20 }}> Time Duration </Text>
-              <Picker
-                style={{ height: 50, width: 25 }}
-                selectedValue={timeInMsecs}
-                mode='dropdown'
-                onValueChange={(itemValue, itemIndex) =>
-                  setTimeInMsecs(itemValue)
-                }>
-                <Picker.Item label="5" value={5} />
-                <Picker.Item label="10" value={10} />
-                <Picker.Item label="20" value={20} />
-                <Picker.Item label="30" value={30} />
-                <Picker.Item label="40" value={40} />
-                <Picker.Item label="50" value={50} />
-                <Picker.Item label="60" value={60} />
-              </Picker>
+              <Text style={timeDurationButtonPressed ? styles.timerButtonPressedText : styles.buttonText}> Time Duration</Text>
+              <TouchableHighlight
+                onPressOut={() => {
+                  setTimeDurationButtonPressed(false);
+                }}
+                onPressIn={() => {
+                  setTimeDurationButtonPressed(true);
+                }}
+              >
+                <Picker
+                  style={{ height: 50, width: 25 }}
+                  selectedValue={timeInMsecs}
+                  mode='dropdown'
+                  onValueChange={(itemValue, itemIndex) =>
+                    setTimeInMsecs(itemValue)
+                  }>
+                  <Picker.Item label="5" value={5} />
+                  <Picker.Item label="10" value={10} />
+                  <Picker.Item label="20" value={20} />
+                  <Picker.Item label="30" value={30} />
+                  <Picker.Item label="40" value={40} />
+                  <Picker.Item label="50" value={50} />
+                  <Picker.Item label="60" value={60} />
+                </Picker>
+              </TouchableHighlight>
             </View>
             <View style={styles.containerRow}>
-              <Text style={{ fontSize: 20 }}> Time Unit </Text>
-              <Picker
-                style={{ height: 50, width: 25 }}
-                selectedValue={timeType}
-                mode='dropdown'
-                onValueChange={(itemValue, itemIndex) =>
-                  setTimeType(itemValue)
-                }>
-                <Picker.Item label="seconds" value={1000} />
-                <Picker.Item label="minutes" value={60000} />
-                <Picker.Item label="hours" value={3600000} />
-              </Picker>
+              <Text style={timeUnitButtonPressed ? styles.timerButtonPressedText : styles.buttonText}> Time Unit </Text>
+              <TouchableHighlight
+                onPressOut={() => {
+                  setTimeUnitButtonPressed(false);
+                }}
+                onPressIn={() => {
+                  setTimeUnitButtonPressed(true);
+                }}
+              >
+                <Picker
+                  style={{ height: 50, width: 25 }}
+                  selectedValue={timeType}
+                  mode='dropdown'
+                  onValueChange={(itemValue, itemIndex) =>
+                    setTimeType(itemValue)
+                  }>
+                  <Picker.Item label="seconds" value={1000} />
+                  <Picker.Item label="minutes" value={60000} />
+                  <Picker.Item label="hours" value={3600000} />
+                </Picker>
+              </TouchableHighlight>
             </View>
           </View>
         </View>
