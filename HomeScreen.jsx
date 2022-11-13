c8ef4c9a967f4668fbe1402081ce5c1779d387b1
JACOB SINGER <jacobbradleysinger@gmail.com>
2022-11-13 12:22:31 -0600

    created dialog for getting music files, basic template

--- HomeScreen.jsx
+++ HomeScreen.jsx
@@ -1,14 +1,41 @@
-import { View, Text, SafeAreaView, TouchableHighlight } from 'react-native';
-import React, { useState } from 'react';
+import { View, Text, SafeAreaView, TouchableHighlight, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { styles } from './HomeScreenStyles';
 import TimerContainer from './TimerContainer';
 import Dialog from "react-native-dialog";
 import IconFontAwesome5Design from 'react-native-vector-icons/FontAwesome5';
+import * as MediaLibrary from 'expo-media-library';
 
 const HomeScreen = () => {
     const [displayApp, setDisplayApp] = useState(false);
     const [dialogActive, setDialogActive] = useState(false);
     const [musicRowAmount] = useState([1, 2, 3, 4, 5, 6]);
+    const [hasMediaLibPermsssions, setHasMediaLibPermissions] = useState(false);
+    const [audioFiles, setAudioFiles] = useState([]);
+
+    const getAudioFiles = async () => {
+        let result = await MediaLibrary.getAssetsAsync({
+            mediaType: 'audio',
+        });
+
+        setAudioFiles(result);
+    };
+
+    useEffect(() => {
+        (async () => {
+            const mediaLibStatus = await MediaLibrary.requestPermissionsAsync();
+            setHasMediaLibPermissions(mediaLibStatus.status === "granted");
+        })();
+    }, []);
+
+    useEffect(() => {
+        if (hasMediaLibPermsssions) {
+            getAudioFiles();
+        }
+    }, [hasMediaLibPermsssions]);
+
+    if (!hasMediaLibPermsssions)
+        return <View></View>;
 
     return (
         <SafeAreaView style={styles.container}>
@@ -28,18 +55,13 @@ const HomeScreen = () => {
                                 </View>
                                 <View style={{ ...styles.containerColumn, paddingBottom: 10, paddingHorizontal: 5 }}>
                                     <TouchableHighlight onPress={() => setDialogActive(!dialogActive)}>
-                                        <Text>Select Ringtone from App</Text>
-                                    </TouchableHighlight>
-                                </View>
-                                <View style={{ ...styles.containerColumn, paddingBottom: 10, paddingHorizontal: 5 }}>
-                                    <TouchableHighlight onPress={() => setDialogActive(!dialogActive)}>
-                                        <Text> Select Ringtone From Device</Text>
+                                        <Text>Select Ringtone</Text>
                                     </TouchableHighlight>
                                 </View>
                             </View>
                             <View>
                                 <Dialog.Container visible={dialogActive}>
-                                    <Dialog.Title>Video Files</Dialog.Title>
+                                    <Dialog.Title>Audio Files</Dialog.Title>
                                     {musicRowAmount.map(num => {
                                         return (
                                             <View key={num} style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 85, alignItems: 'center', right: 100 }}>
@@ -53,8 +75,15 @@ const HomeScreen = () => {
                                         );
                                     })}
                                     <Dialog.Description>
-                                        Please select a ringtone
+                                        Choose from either our library or your device
                                     </Dialog.Description>
+                                    <ScrollView>
+                                        {audioFiles && audioFiles?.assets?.map(audio => {
+                                            return (
+                                                <Text>{audio.filename}</Text>
+                                            );
+                                        })}
+                                    </ScrollView>
                                     <Dialog.Button label="Close" onPress={() => setDialogActive(false)} />
                                 </Dialog.Container>
                             </View>
